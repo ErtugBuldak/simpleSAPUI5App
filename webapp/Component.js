e7144e7226e13ef6f6bcb036fd4ed6151413a47e
ErtugBuldak <ertugbuldak1907@gmail.com>
2026-03-25 10:16:49 +0300

    Moved configurations of the app to manifest.json

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,20 +1,15 @@
 sap.ui.define(
   [
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/model/json/JSONModel"
   ],
-  (UIComponent, JSONModel, ResourceModel) => {
+  (UIComponent, JSONModel) => {
     "use strict";
 
     return UIComponent.extend("ui5.walkthrough.Component", {
       metadata: {
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        rootView: {
-          viewName: "ui5.walkthrough.view.App",
-          type: "XML",
-          id: "app",
-        },
+        manifest: "json"
       },
 
       init() {
@@ -28,12 +23,6 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.setModel(oModel);
-
-        // set i18n model
-        const i18nModel = new ResourceModel({
-          bundleName: "ui5.walkthrough.i18n.i18n",
-        });
-        this.setModel(i18nModel, "i18n");
       },
     });
   },
